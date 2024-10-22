2216ec2d80ffe01830389f8c27878fd7a91b5107
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-10-22 11:39:44 +0200

    Updated height of Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import CardSlider from "./CardSlider";
 
 const Projects = () => {
   return (
-    <section id="projects" className="w-screen h-dvh order-3">
+    <section id="projects" className="w-screen h-screen order-3">
       <h1 className="text-center text-5xl font-bold m-10">My Projects</h1>
       <h2 className="text-center text-xl font-bold m-10">This is a selection of my projects as a web developer.</h2>
       <CardSlider />
